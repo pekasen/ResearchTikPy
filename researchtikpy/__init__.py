b26ebdea8867a5735dc418f33f2b0be48225518b
Philipp Kessling <p.kessling@leibniz-hbi.de>
2024-11-18 11:25:37 +0100

    Add AccessToken class. This automatically refreshes the token when it expires.

--- researchtikpy/__init__.py
+++ researchtikpy/__init__.py
@@ -1,14 +1,25 @@
-from .researchtik import get_access_token, get_followers, get_following, get_liked_videos, get_pinned_videos, get_users_info, get_video_comments, get_videos_hashtag, get_videos_info, get_videos_query
+from .researchtik import (
+    get_followers,
+    get_following,
+    get_liked_videos,
+    get_pinned_videos,
+    get_users_info,
+    get_video_comments,
+    get_videos_hashtag,
+    get_videos_info,
+    get_videos_query,
+)
+from .utils import AccessToken
 
 __all__ = [
-    'get_access_token',
-    'get_following',
-    'get_followers',
-    'get_liked_videos',
-    'get_pinned_videos',
-    'get_users_info',
-    'get_video_comments',
-    'get_videos_hashtag',
-    'get_videos_query',
-    'get_videos_info',
+    "AccessToken",
+    "get_following",
+    "get_followers",
+    "get_liked_videos",
+    "get_pinned_videos",
+    "get_users_info",
+    "get_video_comments",
+    "get_videos_hashtag",
+    "get_videos_query",
+    "get_videos_info",
 ]
